d5abdd1f526414881c29f30e5a5e5c9e876c86a6
Ekefan <eebenezer949@gmail.com>
2024-06-05 17:17:34 +0100

    feat: change create arg shortcode to owner

--- database/urlStore.go
+++ database/urlStore.go
@@ -1,24 +1,25 @@
 package database
 
-//createURLArgs: struct for holding args for creating a new URL-row
+// createURLArgs: struct for holding args for creating a new URL-row
 type CreateURLArgs struct {
-	ShortCode string
-	LongURL string
+	Owner     uint `json:"owner"`
+	ShortCode string `json:"short_code"`
+	LongURL   string `json:"long_url"`
 }
 
 type GetURLArgs struct {
-	ShortCode string
+	ShortCode string `json:"short_code"`
 }
 
 // RunMigrations runs database migrations if the table url don't exist yet
-func (s *Query) CreateURL(args CreateURLArgs) (URL, error){
-	url_row := URL{ShortCode: args.ShortCode, LongURL: args.LongURL}
+func (s *Query) CreateURL(args CreateURLArgs) (URL, error) {
+	url_row := URL{Owner: args.Owner,ShortCode: args.ShortCode, LongURL: args.LongURL}
 	result := s.db.Create(&url_row)
 	return url_row, result.Error
 }
 
-//GetURL: makes a query to the database and returns the URL with short_code specified in args
-func (s *Query) GetURL(args GetURLArgs) (URL, error){ 
+// GetURL: makes a query to the database and returns the URL with short_code specified in args
+func (s *Query) GetURL(args GetURLArgs) (URL, error) {
 	urlRow := URL{}
 	result := s.db.Where("short_code = ?", args.ShortCode).First(&urlRow)
 	return urlRow, result.Error
@@ -29,9 +30,6 @@ func (s *Query) GetURL(args GetURLArgs) (URL, error){
 // TODO: implement updateURL and DeleteURL when You implement USER model
 // func (s *Store) UpdateURL() (url URL)       { return }
 // func (s *Store) DeleteURl()                 {}
-
-
-
 
 // TODO: Implement userStore.go
 //CreateUser
