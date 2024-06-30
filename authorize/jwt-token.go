435a26fe097d9d68e7e08384ceb5f45305e3b2c8
Ebenezer <eebenezer949@gmail.com>
2024-06-30 17:53:26 +0100

    feat: verify jwt

--- authorize/jwt-token.go
+++ authorize/jwt-token.go
@@ -7,22 +7,22 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-type jwtToken struct {
+type JwtMaker struct {
 	key string
 }
 
 const minSecretKeySize = 32
 
-func NewJwtToken(secretKey string)(*jwtToken, error) {
+func NewJwtToken(secretKey string) (*JwtMaker, error) {
 	if len(secretKey) < minSecretKeySize {
 		return nil, fmt.Errorf("invalid key size: must be at least %d characters", minSecretKeySize)
 	}
-	return &jwtToken{
+	return &JwtMaker{
 		key: secretKey,
 	}, nil
 }
 
-func (t *jwtToken)CreateToken(username string, duration time.Duration) (string, error) {
+func (t *JwtMaker) CreateToken(username string, duration time.Duration) (string, error) {
 	payload, err := NewPayload(username, duration)
 	if err != nil {
 		return "", fmt.Errorf("couldn't generate payload id: %v", err)
@@ -36,5 +36,33 @@ func (t *jwtToken)CreateToken(username string, duration time.Duration) (string, 
 	return tokenSigned, nil
 }
 
-//token verification
-// func (t *jwtTOken) VerifyToken(f)(*Payload, error)
+// token verification
+func (t *JwtMaker) VerifyToken(token string) (*Payload, error) {
+	// keyfunc checks the method used for signing the parsed
+	// token in the header. If it is not the same then it is
+	keyfunc := func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, ErrInvalidToken
+		}
+		return t.key, nil
+	}
+	jwtToken, err := jwt.ParseWithClaims(token, &Payload{}, keyfunc)
+	if err != nil {
+		// golang's implementation of jwt returns ErrInvalidClaims error on error
+		return nil, err
+	}
+
+	//get claims and return it
+	payload, ok := jwtToken.Claims.(*Payload)
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+	return payload, nil
+}
+
+//Write test for tokens
+// create a username, duration,
+// create a new token, using a random key
+// case one: Happy case, Verify the token,
+// check the contents of the payload
