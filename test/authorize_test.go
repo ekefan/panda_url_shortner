e967a8a2f39c08e7389b3a86e52f9f7a89604da3
Ebenezer <eebenezer949@gmail.com>
2024-06-30 23:00:48 +0100

    test: create, verify token

---
+++ test/authorize_test.go
@@ -0,0 +1,35 @@
+package test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/ekefan/panda_url_shortner/authorize"
+	"github.com/ekefan/panda_url_shortner/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateToken(t *testing.T) {
+	key := "12345678901234567890123456789012"
+	randomUser, err := util.RandomShortCode(4)
+	require.NoError(t, err)
+	require.NotEmpty(t, randomUser)
+	duration := 3 * time.Minute
+
+	//Case One: Happy case token is created token is verified
+	tokenMaker, err := authorize.NewJwtToken(key)
+	require.NoError(t, err)
+	require.NotEmpty(t, tokenMaker)
+
+	token, err := tokenMaker.CreateToken(randomUser, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := tokenMaker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+	require.NotEmpty(t, payload.ID)
+	require.Equal(t, payload.Owner, randomUser)
+	require.WithinDuration(t, payload.Iat, time.Now(), time.Second)
+
+}
