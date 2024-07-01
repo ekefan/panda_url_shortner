5da6ff81395579db473072116f447056610d5266
Ebenezer <eebenezer949@gmail.com>
2024-07-01 13:23:10 +0100

    feat: implement password hashing

---
+++ util/password.go
@@ -0,0 +1,28 @@
+package util
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPassword takes a password string and creates a hash for it
+func HashPassword(password string)(string, error){
+	if len(password) < 6 {
+		return "", fmt.Errorf("len of password too short: needs at least len of %v", 6)
+	}
+	byteHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(byteHash), nil
+}
+
+// Checks a password matches the stored hash in the database
+func VerifyPassword(hash, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return err
+	}
+	return nil
+}
