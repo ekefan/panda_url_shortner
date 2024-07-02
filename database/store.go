d1b2d7aef8768a3d6cc9b1d8d1d05658c715c5a4
Ebenezer <eebenezer949@gmail.com>
2024-07-02 16:51:10 +0100

    db: update store interface to implement updateTx & get urls

--- database/store.go
+++ database/store.go
@@ -2,20 +2,19 @@ package database
 
 import "gorm.io/gorm"
 
-
-type Store interface{
+type Store interface {
 	CreateURL(args CreateURLArgs) (URL, error)
 	GetURL(args GetURLArgs) (URL, error)
 	RunMigrations(db *gorm.DB, flag int) error
 	CreateUser(args CreateUserArgs) (USER, error)
 	GetUser(args GetUserArgs) (USER, error)
-
+	GetURLs(args GetURLsArg) ([]URL, error)
+	TxUpdateShortCode(args TxUrlArgs) (URL, error)
 }
 
 type Query struct {
 	db *gorm.DB
 }
-
 
 func NewStore(db *gorm.DB) Store {
 	return &Query{
