6d6add283ddb196ffe7def27ec6e20aa683856e9
Ebenezer <eebenezer949@gmail.com>
2024-07-02 04:05:46 +0100

    db: handle updating url shortCode

--- database/urlStore.go
+++ database/urlStore.go
@@ -1,5 +1,16 @@
 package database
 
+import (
+	// "errors"
+
+	"fmt"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+// const minShortCodeLen int = 4
+
 // createURLArgs: struct for holding args for creating a new URL-row
 type CreateURLArgs struct {
 	Owner     string `json:"owner"`
@@ -13,7 +24,7 @@ type GetURLArgs struct {
 
 // RunMigrations runs database migrations if the table url don't exist yet
 func (s *Query) CreateURL(args CreateURLArgs) (URL, error) {
-	url_row := URL{Owner: args.Owner,ShortCode: args.ShortCode, LongURL: args.LongURL}
+	url_row := URL{Owner: args.Owner, ShortCode: args.ShortCode, LongURL: args.LongURL}
 	result := s.db.Create(&url_row)
 	return url_row, result.Error
 }
@@ -25,14 +36,54 @@ func (s *Query) GetURL(args GetURLArgs) (URL, error) {
 	return urlRow, result.Error
 }
 
-//posible upgrade.. getURL only where the deletedAt is zerovalue
+// Code from yesterday
 
-// TODO: implement updateURL and DeleteURL when You implement USER model
-// func (s *Store) UpdateURL() (url URL)       { return }
-// func (s *Store) DeleteURl()                 {}
+// GetURL
+func (s *Query) getUrlForUpdate(shortCode string) (URL, error) {
+	urlRow := URL{}
+	result := s.db.Where("owner = ?", shortCode).First(&urlRow)
+	return urlRow, result.Error
 
-// TODO: Implement userStore.go
-//CreateUser
-//Login
+}
+
+type UpdateShortCodeArgs struct {
+	Owner     string `json:"owner"`
+	ShortCode string `json:"short_code"`
+}
+
+// UpdateShortCode a transaction to update the short code off the url
+func (s *Query) UpdateShortCode(args UpdateShortCodeArgs) (URL, error) {
+	urlRow := URL{}
+	txErr := s.db.Transaction(func(tx *gorm.DB) error {
+		result := tx.Model(&urlRow).Where("owner = ?", args.Owner).
+			Updates(URL{ShortCode: args.ShortCode, UpdatedAt: time.Now()})
+
+		// check if the update query was successful
+		if result.Error != nil {
+			return result.Error
+		}
+
+		//check if the update query affected at least on row
+		if result.RowsAffected == 0 {
+			return fmt.Errorf("no rows affected, possibly invalid owner: %v", args.Owner)
+		}
+
+		return nil
+	})
+
+	if txErr != nil {
+		return urlRow, txErr
+	}
+
+	//get the newly update url row by the newShort code
+	urlRow, err := s.getUrlForUpdate(args.ShortCode)
+	if err != nil {
+		return urlRow, err
+	}
+
+	return urlRow, nil
+}
+
+// func (s *Store) DeleteURl() \begin, get the user delete the url based on the url id{}
 //=====edit URL model to contain title and user generated shortcode
 //UPDATEURL should edit the title of the url, and the shortcode.
