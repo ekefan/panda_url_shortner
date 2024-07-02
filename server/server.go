1724c6f59df4513c16542b571a76f5a1d92afa36
Ebenezer <eebenezer949@gmail.com>
2024-07-02 04:06:57 +0100

    server: take note of errors for making database calls

--- server/server.go
+++ server/server.go
@@ -13,13 +13,13 @@ import (
 )
 
 type Server struct {
-	store database.Store
-	router *gin.Engine
-	config util.Config
+	store    database.Store
+	router   *gin.Engine
+	config   util.Config
 	jwtMaker *authorize.JwtMaker
 }
 
-func NewServer(config util.Config) *Server{
+func NewServer(config util.Config) *Server {
 	dbConn, err := gorm.Open(sqlite.Open("app.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatal("error connecting with database", err)
@@ -34,12 +34,11 @@ func NewServer(config util.Config) *Server{
 		log.Fatal("can not create tokens: ", err)
 	}
 	return &Server{
-		store: s,
-		config: config,
+		store:    s,
+		config:   config,
 		jwtMaker: makeJwt,
 	}
 }
-
 
 func (s *Server) StartServer() error {
 	if err := s.router.Run(s.config.ServerAddress); err != nil {
@@ -47,7 +46,6 @@ func (s *Server) StartServer() error {
 	}
 	return nil
 }
-
 
 func (s *Server) SetupRouter() {
 	newRouter := gin.Default()
@@ -58,8 +56,15 @@ func (s *Server) SetupRouter() {
 	routes := newRouter.Use(jwtAuthHandler(*s.jwtMaker))
 	routes.POST("/new", s.shortenURL)
 	routes.GET("/:short_code", s.goToURL)
-	
+
+	// ================= URLS ================
+	// 			getUrls --- query..with page size and offset
+	// 			deleteUrl -- delete from url where id = url = ID
+	// 			editshortcode -- transaction, begin,  udpate url. commit ///working on it // done
+
+	// ================= USERs ===============
+	//			updateUser ---transaction // begin update url.owner update user.name commit if any error rollback
+	//			deleteUser --- transaction // begin delete urls where owner = username, delete users where name = username if any error rollback
+
 	s.router = newRouter
 }
-
-
